e0ab117f96483e21321facfc0e4a94220675af3a
Superklez <bjlherrera@gmail.com>
2021-10-07 16:26:24 +0800

    Added DFT orthogonal basis
    Support for the DFT orthogonal basis has not yet been implemented.

--- bases.py
+++ bases.py
@@ -2,9 +2,9 @@ import math
 import numpy as np
 
 def dct2_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Discrete cosine transfor-II (DCT-II) orthogonal basis.
-    '''
+    """
     basis = []
     for k in range(N):
         if k == 0:
@@ -15,9 +15,9 @@ def dct2_basis(N: int, dtype: type = np.float32):
     return np.array(basis, dtype=dtype).T
 
 def dst2_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Discrete sine transform-II (DST-II) basis.
-    '''
+    """
     basis = []
     for k in range(N):
         basis.append(np.sin(math.pi / N * (np.arange(N) \
@@ -25,34 +25,43 @@ def dst2_basis(N: int, dtype: type = np.float32):
     return np.array(basis, dtype=dtype).T
 
 def sin_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Sine subdictionary.
-    '''
+    """
     basis = []
     for k in range(1, math.ceil(N / 2) + 1):
         basis.append(np.sin(2 * np.pi * k * np.linspace(0, 1, N), dtype=dtype))
     return np.array(basis).T
 
 def cos_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Cosine subdictionary.
-    '''
+    """
     basis = []
     for k in range(1, math.ceil(N / 2) + 1):
         basis.append(np.cos(2 * np.pi * k * np.linspace(0, 1, N), dtype=dtype))
     return np.array(basis).T
 
 def poly_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Polynomial subdictionary.
-    '''
+    """
     basis = []
     for n in range(1, 21):
         basis.append(np.power(np.linspace(0, 1, N, dtype=dtype), n - 1))
     return np.array(basis).T
 
 def kd_basis(N: int, dtype: type = np.float32):
-    '''
+    """
     Shifted Kronecker delta subdictionary.
-    '''
+    """
     return np.eye(N, dtype=dtype)
+
+def dft_basis(N: int):
+    """
+    Discrete Fourier transform (DFT) orthogonal basis.
+    """
+    basis = []
+    for k in range(N):
+        basis.append(np.sqrt(1 / N) * np.exp(-2j*np.pi*k*np.arange(N)/N))
+    return np.array(basis).T
